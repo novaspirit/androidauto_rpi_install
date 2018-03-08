d41c97dd724d592f83e00ac829c9c8a4181f52a5
don <novaspirit@novaspirit.com>
2018-03-08 15:58:31 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,5 +14,5 @@ mkdir openauto_build
 cd openauto_build
 cmake -DCMAKE_BUILD_TYPE=Release -DRPI3_BUILD=TRUE -DAASDK_INCLUDE_DIRS="/home/pi/aasdk/include" -DAASDK_LIBRARIES="/home/pi/aasdk/lib/libaasdk.so" -DAASDK_PROTO_INCLUDE_DIRS="/home/pi/aasdk_build" -DAASDK_PROTO_LIBRARIES="/home/pi/aasdk/lib/libaasdk_proto.a" ../openauto
 make -j4
-echo "@/home/pi/openauto/bin/autoapp" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+echo "sudo @/home/pi/openauto/bin/autoapp" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 /home/pi/openauto/bin/autoapp
